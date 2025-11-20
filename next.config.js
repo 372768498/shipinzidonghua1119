efe201b7bf7c4605c9396299b94f42a467fc8be6
372768498 <41884301+372768498@users.noreply.github.com>
2025-11-20 13:46:31 +0800

    fix: 添加TikTok、YouTube等平台的图片域名配置

--- next.config.js
+++ next.config.js
@@ -10,6 +10,45 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'fal.media',
       },
+      // TikTok CDN域名
+      {
+        protocol: 'https',
+        hostname: '*.tiktokcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.tiktokcdn-us.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p16-sign.tiktokcdn-us.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p19-common-sign.tiktokcdn-us.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p77-sign.tiktokcdn-us.com',
+      },
+      // YouTube缩略图
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
+      // Instagram
+      {
+        protocol: 'https',
+        hostname: '*.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.cdninstagram.com',
+      },
     ],
   },
 }
